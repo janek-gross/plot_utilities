a6a1fa21e02672215c0dd845125ee36d13a44917
janek-gross <50371707+janek-gross@users.noreply.github.com>
2020-06-19 11:36:04 +0200

    Update example_plot.py

--- example_plot.py
+++ example_plot.py
@@ -10,12 +10,12 @@ depth = 10
 fig = plt.figure(figsize=(4,4))
 
 plt.imshow(np.ones((400,400))*255, cmap = 'gray', vmin=0, vmax=255)
-image_shape = matplotlib_figure_to_rgb(fig, grayscale=True).shape
+image_shape = figure_to_rgb(fig, grayscale=True).shape
 grayscale_array = np.zeros((depth, *image_shape))
 
 for i in range(depth):
     fig = plt.gcf()
-    grayscale_array[i] = matplotlib_figure_to_rgb(fig, grayscale=True)
+    grayscale_array[i] = figure_to_rgb(fig, grayscale=True)
     plt.imshow(grayscale_array[i], cmap = 'gray')
     
 gif_from_color_array(grayscale_array, scale = 3)
